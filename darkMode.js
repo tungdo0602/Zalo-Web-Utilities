443ba3082ba66bdbc7b7475069b0f787c637fcc4
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-25 22:50:45 +0700

    Update darkMode.js

--- darkMode.js
+++ darkMode.js
@@ -1,5 +1,5 @@
 window.setInterval(()=>{
-    Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.flx-al-c.fake-textholder.truncate.fk-normal)")).map(n=>n.style.backgroundColor="black");
+    Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.flx-al-c.fake-textholder.truncate.fk-normal):not(#scroll-vertical>div)")).map(n=>n.style.backgroundColor="black");
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet)")).map(n=>n.style.color="blue");
     Array.from(document.querySelectorAll("div")).map(n=>n.style.borderColor='blue');
 });
