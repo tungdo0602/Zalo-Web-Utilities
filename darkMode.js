86e69b865d9bbe5d142624c4d1472689a87a0ac7
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-25 21:34:51 +0700

    Create darkMode.js

---
+++ darkMode.js
@@ -0,0 +1,5 @@
+window.setInterval(()=>{
+    Array.from(document.querySelectorAll("div")).map(n=>n.style.color='blue');
+    Array.from(document.querySelectorAll("div")).map(n=>n.style.backgroundColor='black');
+});
+//Lol
