bd3c1663433593cbd90abfcec6dc1bc8263d1916
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-26 08:56:04 +0700

    Update and rename darkMode.js to darkMode.user.js

--- darkMode.user.js
+++ darkMode.user.js
@@ -1,6 +1,21 @@
+// ==UserScript==
+// @name         Zalo Dark Mode
+// @namespace    https://github.com/tungdo0602/Zalo-Web-Utilities/blob/main/darkMode.js
+// @version      0.1
+// @description  Simple Dark Mode
+// @author       tungdo0602 (https://github.com/tungdo0602)
+// @match        *://*.zalo.me/*
+// @match        *://zalo.me/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zalo.me
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
 window.setInterval(()=>{
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.flx-al-c.fake-textholder.truncate.fk-normal):not(#scroll-vertical):not(#scroll-vertical>div)")).map(n=>n.style.backgroundColor="black");
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet)")).map(n=>n.style.color="blue");
     Array.from(document.querySelectorAll("div:not(#scroll-vertical>div):not(#scroll-vertical)")).map(n=>n.style.borderColor='blue');
     Array.from(document.querySelectorAll("#scroll-vertical>div")).map(n=>n.style.backgroundColor='blue');
 });
+})();
