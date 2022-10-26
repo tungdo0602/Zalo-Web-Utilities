60e7ccd12df7edd3edb1eef28c3b67b384168705
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-27 00:17:53 +0700

    Update darkMode.user.js

--- darkMode.user.js
+++ darkMode.user.js
@@ -18,7 +18,7 @@ window.setInterval(()=>{
     Array.from(document.querySelectorAll(".card--link-content")).map(n=>{if(!n.parentElement.className.includes("zlmbox")){n.parentElement.className+=' zlmbox'}});
     Array.from(document.querySelectorAll(".card-send-time")).map(n=>{if(!n.parentElement.className.includes("zlmbox")){n.parentElement.className+=' zlmbox'}});
     /*Global*/
-    Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.flx-al-c.fake-textholder.truncate.fk-normal):not(#scroll-vertical):not(#scroll-vertical>div):not(style):not(script):not(.zl-modal):not(.zl-modal__container):not(.mn-modal):not(.conv-item-setting):not(#resize-handle):not(.conv-unread):not(#main-tab):not(.leftbar-tab):not(.nav__tabs__zalo):not(.card):not(.card-send-time__sendTime):not(.card-send-time):not(.text):not(span-15):not(span-13):not(.zlmbox):not(.quote-banner):not(.card-sender-name):not(.card-sender-name>span):not(.quote-line):not(.quote-banner-content):not(.quote-name-container):not(.quote-name):not(.quote-banner-name):not(.quote-text):not(.text-is-link):not(.card--link-content):not(div-15):not(.card--link-container):not(.card-content):not(span-b14):not(.card--link-img):not(.card-send-status):not(.card-send-status>span):not(.card--undo):not(.card--undo>span)")).map(n=>n.style.backgroundColor='#282c34');
+    Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.flx-al-c.fake-textholder.truncate.fk-normal):not(#scroll-vertical):not(#scroll-vertical>div):not(style):not(script):not(.zl-modal):not(.zl-modal__container):not(.mn-modal):not(.conv-item-setting):not(#resize-handle):not(.conv-unread):not(#main-tab):not(.leftbar-tab):not(.nav__tabs__zalo):not(.card):not(.card-send-time__sendTime):not(.card-send-time):not(.text):not(span-15):not(span-13):not(.zlmbox):not(.quote-banner):not(.card-sender-name):not(.card-sender-name>span):not(.quote-line):not(.quote-banner-content):not(.quote-name-container):not(.quote-name):not(.quote-banner-name):not(.quote-text):not(.text-is-link):not(.card--link-content):not(div-15):not(.card--link-container):not(.card-content):not(span-b14):not(.card--link-img):not(.card-send-status):not(.card-send-status>span):not(.card--undo):not(.card--undo>span):not(.card--link__sub)")).map(n=>n.style.backgroundColor='#282c34');
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.zl-avatar):not(style):not(script):not(.conv-unread)")).map(n=>n.style.color='#abb2bf');
     //Array.from(document.querySelectorAll("div:not(#scroll-vertical>div):not(#scroll-vertical)")).map(n=>n.style.borderColor='#282c34');
     Array.from(document.querySelectorAll("#scroll-vertical")).map(n=>n.style.backgroundColor='#d3d3d3');
@@ -36,6 +36,7 @@ window.setInterval(()=>{
         Array.from(document.querySelectorAll(".quote-banner")).map(n=>n.style.backgroundColor='#21252b');
         Array.from(document.querySelectorAll(".quote-line")).map(n=>n.style.backgroundColor='#21252b');
         Array.from(document.querySelectorAll(".quote-banner-name")).map(n=>n.style.backgroundColor='#21252b');
+        Array.from(document.querySelectorAll(".chat-message__actions")).map(n=>n.style.backgroundColor='#21252b');
     }catch{}
     /*Main 2*/
     try{
