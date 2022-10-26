ffe11c4eab50904987b0f9600cc781abab704952
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-26 19:32:56 +0700

    Update darkMode.user.js

--- darkMode.user.js
+++ darkMode.user.js
@@ -13,7 +13,7 @@
 window.setInterval(()=>{
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.flx-al-c.fake-textholder.truncate.fk-normal):not(#scroll-vertical):not(#scroll-vertical>div)")).map(n=>n.style.backgroundColor='black');
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.zl-avatar)")).map(n=>n.style.color='white');
-    Array.from(document.querySelectorAll("div:not(#scroll-vertical>div):not(#scroll-vertical)")).map(n=>n.style.borderColor='blue');
+    //Array.from(document.querySelectorAll("div:not(#scroll-vertical>div):not(#scroll-vertical)")).map(n=>n.style.borderColor='white');
     Array.from(document.querySelectorAll("#scroll-vertical>div")).map(n=>n.style.backgroundColor='blue');
 });
 })();
