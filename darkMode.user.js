d56bdde033e6f47ddbd547f67ed3e7b50fb88532
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-26 19:00:20 +0700

    Update darkMode.user.js

--- darkMode.user.js
+++ darkMode.user.js
@@ -12,9 +12,25 @@
 
 (function() {
     'use strict';
+if(!document.querySelector("#zlRB")){
+zlRB = document.createElement("style");
+zlRB.id = 'zlRB';
+zlRB.innerText = `
+.rbtext {
+  animation: color-change 2.5s infinite;
+}
+
+@keyframes color-change {
+  0% { color: red; }
+  50% { color: blue; }
+  100% { color: green; }
+}
+`
+document.body.appendChild(zlRB);
+}
 window.setInterval(()=>{
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.flx-al-c.fake-textholder.truncate.fk-normal):not(#scroll-vertical):not(#scroll-vertical>div)")).map(n=>n.style.backgroundColor="black");
-    Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet)")).map(n=>n.style.color="blue");
+    Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet)")).map(n=>n.className+=" rbtext");
     Array.from(document.querySelectorAll("div:not(#scroll-vertical>div):not(#scroll-vertical)")).map(n=>n.style.borderColor='blue');
     Array.from(document.querySelectorAll("#scroll-vertical>div")).map(n=>n.style.backgroundColor='blue');
 });
