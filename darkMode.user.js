0af16a88ddeeafa1a3e454531fc6a7881614a2af
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-26 20:50:02 +0700

    fix

--- darkMode.user.js
+++ darkMode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zalo Dark Mode
 // @namespace    https://github.com/tungdo0602/Zalo-Web-Utilities/blob/main/darkMode.js
-// @version      0.1
+// @version      0.2
 // @description  Simple Dark Mode
 // @author       tungdo0602 (https://github.com/tungdo0602)
 // @match        *://*.zalo.me/*
@@ -13,7 +13,10 @@
 window.setInterval(()=>{
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.flx-al-c.fake-textholder.truncate.fk-normal):not(#scroll-vertical):not(#scroll-vertical>div):not(style):not(script):not(.zl-modal):not(.zl-modal__container):not(.mn-modal)")).map(n=>n.style.backgroundColor='#23272e');
     Array.from(document.querySelectorAll("*:not(.zl-avatar__alphabet):not(.zl-avatar):not(style):not(script)")).map(n=>n.style.color='#abb2bf');
-    Array.from(document.querySelectorAll("div:not(#scroll-vertical>div):not(#scroll-vertical)")).map(n=>n.style.borderColor='#282c34');
+    //Array.from(document.querySelectorAll("div:not(#scroll-vertical>div):not(#scroll-vertical)")).map(n=>n.style.borderColor='#282c34');
     Array.from(document.querySelectorAll("#scroll-vertical>div:not(style):not(script)")).map(n=>n.style.backgroundColor='#d3d3d3');
+    try{
+        document.querySelector(".setting--bottom").parentElement.style.marginRight='-6.25px'
+    }catch{}
 });
 })();
